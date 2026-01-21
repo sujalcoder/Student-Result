499f66370f2c0709d51dcb35e6e72912b3d48312
sujalcoder <sujalnirmal721@gmail.com>
2026-01-21 12:24:49 +0530

    fdg

--- database.py
+++ database.py
@@ -7,10 +7,3 @@ admins=db.admins
 students=db.students    
 marks=db.marks
 teachers=db.teachers
-print(db.list_collection_names())
-students.insert_one({
-    "full_name": "Test Student",
-    "roll_no": "1001",
-    "class": "10",
-    "section": "A"
-})
