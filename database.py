c9de72d11411582dcb414bf839c28cac146a0f1c
sujalcoder <sujalnirmal721@gmail.com>
2026-02-11 18:20:06 +0530

    jh

--- database.py
+++ database.py
@@ -1,11 +1,20 @@
-# from pymongo import MongoClient
-# import os
+import os
+from pymongo import MongoClient
 
-# Mongo_URL = os.environ.get("MONGO_URL")
-# client = MongoClient(Mongo_URL)
-# db = client["student_marks"]
+Mongo_URL = os.environ.get("MONGO_URL")
 
-# admins = db.admins
-# students = db.students
-# marks = db.marks
-# teachers = db.teachers
+try:
+    client = MongoClient(Mongo_URL)
+    db = client["student_marks"]
+
+    admins = db.admins
+    students = db.students
+    marks = db.marks
+    teachers = db.teachers
+
+except Exception as e:
+    print("MongoDB Connection Failed:", e)
+    admins = None
+    students = None
+    marks = None
+    teachers = None
