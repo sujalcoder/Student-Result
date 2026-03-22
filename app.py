aacd333957ef138883b32e4d1913f324f574032c
sujalcoder <sujalnirmal721@gmail.com>
2026-03-22 09:43:21 +0530

    f

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from teacher import teacher_bp
 from student import students_bp
 from admin import admin_bp
-import os
+
 
 app = Flask(__name__)
 app.register_blueprint(students_bp)
@@ -11,7 +11,4 @@ app.register_blueprint(admin_bp)
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-
-    app.run(host="0.0.0.0", port=port)
-app = app
+    app.run(host="0.0.0.0", debug=True)
